47249985e90d1961babe6c216a544ccae00710b0
20210298 <144835269+20210298@users.noreply.github.com>
2024-04-04 13:43:41 +0900

    Update FruitBuyer.h

--- untitled/FruitBuyer.h
+++ untitled/FruitBuyer.h
@@ -4,7 +4,7 @@
 class FruitBuyer {
 private:
     int money; //변수명을 myMoney -> money로 변경
-    int numOfApples=0;  //생성자로 초기화하면 0으로 초기화가 안됨
+    int numOfApples=0;
 public:
     FruitBuyer();  //멤버   money를   0원으로   초기화
     FruitBuyer(int money); //멤버   money를   money로   초기화
